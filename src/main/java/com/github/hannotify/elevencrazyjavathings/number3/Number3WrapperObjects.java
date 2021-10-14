2623840653b38df7332efa9cefdd2ce4566c9892
Hanno Embregts <hanno.embregts@infosupport.com>
2021-10-14 14:56:30 +0200

    #3- Wrapper objects: some are more equal than others

---
+++ src/main/java/com/github/hannotify/elevencrazyjavathings/number3/Number3WrapperObjects.java
@@ -0,0 +1,7 @@
+package com.github.hannotify.elevencrazyjavathings.number3;
+
+public class Number3WrapperObjects {
+    public static boolean integerEquals(Integer a, Integer b) {
+        return a == b;
+    }
+}
---
+++ src/test/java/com/github/hannotify/elevencrazyjavathings/number3/Number3WrapperObjectsTest.java
@@ -0,0 +1,62 @@
+package com.github.hannotify.elevencrazyjavathings.number3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static com.github.hannotify.elevencrazyjavathings.number3.Number3WrapperObjects.integerEquals;
+
+@DisplayName("#3 - Wrapper objects: some are more equal than others")
+class Number3WrapperObjectsTest {
+
+    @Test
+    @DisplayName("integerEquals() should return false when different arguments are passed")
+    void integerEqualsShouldReturnFalseWithDifferentArguments() {
+        assertThat(integerEquals(200, 300)).isFalse();
+    }
+
+    /*
+    All wrapper objects are immutable.
+    When you do `i++`, what actually happens is something like this:
+
+        `i = Integer.valueOf( i.intValue()  + 1);`
+
+    As you can see, a different Integer object is assigned back to i.
+     */
+
+    @Test
+    @DisplayName("integerEquals() should return false when the same arguments (200) are passed")
+    void integerEqualsShouldReturnFalseWith200() {
+        assertThat(integerEquals(200, 200)).isFalse();
+    }
+
+    @Test
+    @DisplayName("integerEquals() should return false when the same arguments (10) are passed")
+    void integerEqualsShouldReturnFalseWith10() {
+        assertThat(integerEquals(10, 10)).isFalse();
+    }
+
+    /*
+    However, to save on memory, Java 'reuses' all the wrapper objects whose values fall in the following ranges:
+
+    * All Boolean values (true and false)
+    * All Byte values
+    * All Character values from \u0000 to \u007f (i.e. 0 to 127 in decimal)
+    * All Short and Integer values from -128 to 127
+
+    So ==  will always return true when their primitive values are the same and belong to the above list of values.
+     */
+
+    @Test
+    @DisplayName("integerEquals() should return true when the same arguments (10) are passed but one of the arguments is a new instance")
+    void integerEqualsShouldReturnTrueWith10AndNew10() {
+        assertThat(integerEquals(10, new Integer(10))).isTrue();
+    }
+
+    @Test
+    @DisplayName("integerEquals() should return true when the same arguments (10) are passed but one of the arguments is created using Integer.valueOf()")
+    void integerEqualsShouldReturnTrueWith10AndValueOf10() {
+        assertThat(integerEquals(10, Integer.valueOf(10))).isTrue();
+    }
+
+}
