7d99f37152036da535d2a91c438e920564fa0842
Hanno Embregts <hanno.embregts@infosupport.com>
2021-10-31 20:22:08 +0100

    #9 - Simplify things

--- src/main/java/com/github/hannotify/elevencrazyjavathings/number9/Number9AccessingStaticInterfaceMethods.java
+++ src/main/java/com/github/hannotify/elevencrazyjavathings/number9/Number9AccessingStaticInterfaceMethods.java
@@ -1,16 +1,26 @@
 package com.github.hannotify.elevencrazyjavathings.number9;
 
-public class Number9AccessingStaticInterfaceMethods {
-    public static void startMeetup() {
-        var utrechtJugMeetup = new Meetup();
-        System.out.println(utrechtJugMeetup.welcome());
-//        System.out.println(utrechtJugMeetup.start());
-    }
+import java.time.LocalTime;
 
-    public static void startConference() {
-        var jFall = new Conference();
-        System.out.println(jFall.welcome());
-        System.out.format("This is the %s edition.%n", jFall.YEAR);
-//        System.out.println(jFall.start());
+public class Number9AccessingStaticInterfaceMethods {}
+
+interface Slot {
+    int LENGTH_IN_MINUTES = 50;
+
+    static String lengthDescription() {
+        return String.format("This slot lasts %d minutes.", LENGTH_IN_MINUTES);
     }
 }
+
+class Talk implements Slot {
+    private final String speaker;
+    private final String title;
+    private final LocalTime startTime;
+
+    public Talk(String speaker, String title, LocalTime startTime) {
+        this.speaker = speaker;
+        this.title = title;
+        this.startTime = startTime;
+    }
+}
+
--- src/test/java/com/github/hannotify/elevencrazyjavathings/number9/Number9AccessingStaticInterfaceMethodsTest.java
+++ src/test/java/com/github/hannotify/elevencrazyjavathings/number9/Number9AccessingStaticInterfaceMethodsTest.java
@@ -1,47 +1,19 @@
 package com.github.hannotify.elevencrazyjavathings.number9;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import java.time.LocalTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DisplayName("#9 - Accessing static interface methods: a lot harder than you might think")
 class Number9AccessingStaticInterfaceMethodsTest {
-    private final PrintStream standardOut = System.out;
-    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-
-    @BeforeEach
-    void setUp() {
-        System.setOut(new PrintStream(outputStreamCaptor));
-    }
-
     @Test
-    @DisplayName("startMeetup() should print a welcome message and an opening keynote message")
-    void startMeetup() {
-        Number9AccessingStaticInterfaceMethods.startMeetup();
+    @DisplayName("Talk length description should be the right value")
+    void testAccessingStaticInterfaceMethod() {
+        Talk carrotsAreAwesome = new Talk("Bugs Bunny", "Carrots Are Awesome!", LocalTime.of(11, 0));
 
-        assertThat(outputStreamCaptor.toString()).contains(
-                "Welcome to this meetup!",
-                "Time for the opening keynote.");
-    }
-
-    @Test
-    @DisplayName("startConference() should print a welcome message and an opening keynote message")
-    void startConference() {
-        Number9AccessingStaticInterfaceMethods.startConference();
-
-        assertThat(outputStreamCaptor.toString()).contains(
-                "Welcome to this conference!",
-                "This is the 2021 edition.",
-                "Time for the opening keynote.");
-    }
-
-    @AfterEach
-    void tearDown() {
-        System.setOut(standardOut);
+        assertThat(carrotsAreAwesome.LENGTH_IN_MINUTES).isEqualTo(50);
+//        assertThat(carrotsAreAwesome.lengthDescription()).isEqualTo("This slot lasts 50 minutes.");
     }
 }
